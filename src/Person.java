7f9e32db2fba1a079ce3a47b681534f4b687f7eb
Merhawi Tesfay <teweldemerhawi95@gmail.com>
2024-04-01 03:37:07 -0400

    First Commit

---
+++ src/Person.java
@@ -0,0 +1,18 @@
+public class Person {
+    public static void main(String[] args) {
+        
+// Initializing variables
+        String firstName = "John";
+        String lastName = "Smith";
+        String streetAddress = "1800 Main St.";
+        String city = "Chicago";
+        int zipCode = 60601;
+        
+// Printing values
+        System.out.println("First name: " + firstName);
+        System.out.println("Last name: " + lastName);
+        System.out.println("Street address: " + streetAddress);
+        System.out.println("City: " + city);
+        System.out.println("Zip code: " + zipCode);
+    }
+}
